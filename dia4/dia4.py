500eaa844aad3fa18aac236d0b2aaf9fc8b5b1d5
le <conexionbeta@yahoo.es>
2021-02-10 18:50:45 -0500

    dia 4 y soluciones

--- dia4/dia4.py
+++ dia4/dia4.py
@@ -1,27 +1,45 @@
 """ LISTAS """
-""" añadir con
+""" 
+añadir:
     .append()
     + 
-borrar
+borrar:
     .pop()
 
-eliminar lista
-    del lista
+eliminar lista:
+    del
 
-vacear
+vacear:
    .clear()
 
 slice (trozo de lista)
 
-longitud
+longitud:
     len()
-    
-    """
+"""
+
+"""
 ejemplos = ["rojo", "blanco", "verde", "azul"]
-for color in range(len(ejemplos)):
-    print(ejemplos(color))
+ r=range(len(ejemplos)) #es de tipo rango
+l=list(r) #crea lista a partir de r
+print(l)
+print(type(l)) #l!=r, son datos de tipo diferente
+ """
+
+#imprime los índices
+ejemplo = ["rojo", "amarillo", "verde", "azul"]
+for color in range(len(ejemplo)):
+    print(color)
+
+#imprime los elementos
+ejemplo1 = ["rojo", "blanco", "verde", "azul"]
+for color in range(len(ejemplo1)):
+    print(ejemplo1[color])
+
+
+
 
 """ zip (es un objeto) para emparejar"""
-altura=[130, 145, 187, 175, 156]
+""" altura=[130, 145, 187, 175, 156]
 nombre =["dani", "ricardo", "juan", "carla"]
-print(list(zip(altura, nombre)))
+print(list(zip(altura, nombre))) """
--- dia4/dia4Pro.py
+++ dia4/dia4Pro.py
@@ -1,37 +1,80 @@
-ejemplos = ["rojo", "blanco", "verde", "azul"]
-
-""" for coso in ejemplos:
-    print(coso)
+##24 imprime los >10
+""" 
+nums = [1, 23, 5, 8, 40, 12, 2, 67, 24, 9, 39]
+for indice in range(len(nums)):
+  if nums[indice]>10:
+   print(nums[indice])
  """
-r=range(len(ejemplos)) #es de tipo rango
-l=list(r) #crea lista a partir de r
-print(l)
-print(type(l)) #l!=r, son datos de tipo diferente
-
-""" for coso in range(len(ejemplos)):
-    print(ejemplos(coso))
+""" # otra solucion
+nums = [1, 23, 5, 8, 40, 12, 2, 67, 24, 9, 39]
+for indice in nums:  
+  if indice>10: print(indice)
+ """  
+##23 retorna último elemento de lista
+""" 
+def ulti(lita):
+  if lita==[]:
+    return "lista vacía"
+  else:
+    l=len(lita)
+    return lita[l-1]
+print(ulti([5, 1, 8, 7])) # 7
+print(ulti([])) # "La lista está vacía"
  """
 
-""" def derango(r):
-   
-    for element 
-    
-    return range(r)
-  """    
+##22 ordena lista de números
+## d es el índice o ubicación dentro de la lista, y va de 0 hasta longitud-1
+""" 
+def orde(lita):
+  for d in range(len(lita)-1): #este for repite el otro for, l veces, donde l=longitud
+    for d in range(len(lita)-1): #adentro de este for se intercambian pares contiguos
+      if lita[d]>lita[d+1]:
+        temp=lita[d]
+        lita[d]=lita[d+1]
+        lita[d+1]=temp
+  return lita
+print(orde([5,12,4,6,8,1,0,-1,3,12,15,17,-3]))
+"""
 
-#print(derango(5)) # [1, 2, 3, 4, 5]
-""" print(crear_rango(1)) # [1]
-print(crear_rango(0)) # []
-print(crear_rango(-10)) # []
 
-x=range(8)
-
-print(x)
+#21 combinar dos listas en una nueva combinada en tuplas
+""" 
+def combina(lista1, lista2):
+  combi=list(zip(lista1, lista2))
+  return combi
+print(combina([1, 2], ['a', 'b'])) # [(1, 'a'), (2, 'b')]
+print(combina(["Pedro", "Maria"], [15, 22])) # [("Pedro", 15), ("Maria", 22)]
  """
-""" 18 insertar elemento e imprimir verticalmente """
-""" nombres = ["Pedro", "Maria", "Pablo", "Diana"]
+#20 recibe lista devuelve longitud, una solución es casi que reutilizar len(lista)
 
-nombres.append("Juan")
+#19
+""" reciba un número y retorne una lista desde 1 hasta el número recibido"""
+"""
+def derango(digito):
+  lita=[]
+  for i in range(digito): #range(digito)<->range(0,digito): i en intervalo de 0 a digito
+    lita.append(i+1)
+  return lita
+print(derango(8))
+ """
+
+#18 
+""" insertar elemento e imprimir verticalmente """
+""" 
+nombres = ["Pedro", "Maria", "Pablo", "Diana"]
+nombres.append("juanita limaña")
+for j in nombres:
+  print(j)
+ """
+##otra solucion
+""" 
+nombres = ["Pedro", "Maria", "Pablo", "Diana"]
+nombres=nombres+["Pedrina"] # nombres+=["Pedrina"] 
+for j in nombres:
+  print(j)
+ """
+##solucion rudimentaria
+""" nombres.append("Juan")
 print(nombres[0])
 print(nombres[1])
 print(nombres[2])
